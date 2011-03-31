5a055fe8dea52c98178e3d2cf07c43b5299cae24
Scott VonSchilling <scottvonschilling@gmail.com>
2011-03-31 17:07:07 -0400

    Tiger and Mimi images added

--- createNewSession.php
+++ createNewSession.php
@@ -93,7 +93,7 @@ $settings = array (	"userName" => "Wild_Tiger",
 					"bgColor" => "#FFFFFF",
 					"bio" => "I'm Scott VonSchilling's tabby cat.",
 					"location" => "Princeton, NJ",
-					"imageUrl" => "http://scottvonschilling.com/tiger_normal.jpg"
+					"imageUrl" => "./images/tiger_normal.jpg"
 			);
 $tiger->setSettings($settings);
 $tiger->saveToDB($sql,$id);
@@ -106,7 +106,7 @@ $settings = array (	"userName" => "Classy_Mimi",
 					"bgColor" => "#FFFFFF",
 					"bio" => "I am NOT Scott VonSchilling's cat. Scott VonSchilling is my human.",
 					"location" => "Princeton, NJ",
-					"imageUrl" => "http://scottvonschilling.com/mimi_normal.jpg"
+					"imageUrl" => "./images/mimi_normal.jpg"
 			);
 $mimi->setSettings($settings);
 $mimi->saveToDB($sql,$id);
